9a86e5d5a1ca02cf8f9931a1bb7c531352c0aa75
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2022-04-21 18:01:45 +0200

    fix code smell

--- src/test/java/com/farao_community/farao/gridcapa/export/GridcapaExportServiceTest.java
+++ src/test/java/com/farao_community/farao/gridcapa/export/GridcapaExportServiceTest.java
@@ -3,8 +3,6 @@ package com.farao_community.farao.gridcapa.export;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,12 +11,6 @@ class GridcapaExportServiceTest {
 
     @Autowired
     private GridcapaExportService outputsToFtpService;
-
-    @MockBean
-    private FtpClientAdapter ftpClientAdapter;
-
-    @MockBean
-    private RestTemplate restTemplate;
 
     @Test
     void checkUnknownEventThrowsException() {
@@ -38,20 +30,4 @@ class GridcapaExportServiceTest {
         outputsToFtpService.convertEvent(taskStatusUpdateEvent);
         assertDoesNotThrow(() -> RuntimeException.class);
     }
-
-    //TODO
-    /*
-    @Test
-    void checkSuccessfulTaskHandling() {
-        TaskStatusUpdate taskStatusUpdate = new TaskStatusUpdate(UUID.randomUUID(), TaskStatus.SUCCESS);
-        try {
-            Mockito.when(restTemplate.getForEntity(Mockito.any(), Mockito.any())).thenReturn(ResponseEntity.accepted().build());
-            Mockito.doNothing().when(ftpClientAdapter).open();
-            Mockito.doNothing().when(ftpClientAdapter).upload(Mockito.any(), Mockito.any());
-            Mockito.doNothing().when(ftpClientAdapter).close();
-        } catch (IOException ignored) {
-        }
-        outputsToFtpService.transferSuccessfulTasksOutputs(taskStatusUpdate);
-        assertDoesNotThrow(() -> RuntimeException.class);
-    }*/
 }
