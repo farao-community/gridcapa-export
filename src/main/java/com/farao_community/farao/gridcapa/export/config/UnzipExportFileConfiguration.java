15bc6c50575323272fa5e3ed9ef1fb5e770b1986
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2024-06-14 14:56:43 +0200

    Add copyright

--- src/main/java/com/farao_community/farao/gridcapa/export/config/UnzipExportFileConfiguration.java
+++ src/main/java/com/farao_community/farao/gridcapa/export/config/UnzipExportFileConfiguration.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.gridcapa.export.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -5,6 +11,9 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
+/**
+ * @author Marc Schwitzguébel {@literal <marc.schwitzguebel at rte-france.com>}
+ */
 @Configuration
 @ConfigurationProperties(prefix = "export")
 public class UnzipExportFileConfiguration {
